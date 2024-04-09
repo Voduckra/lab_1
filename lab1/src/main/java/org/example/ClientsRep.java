dae590b090b6627b8ecab2becfd8aa03d3c9214a
Voduckra <135720709+Voduckra@users.noreply.github.com>
2024-04-09 19:55:09 +0500

    Update ClientsRep.java

--- lab1/src/main/java/org/example/ClientsRep.java
+++ lab1/src/main/java/org/example/ClientsRep.java
@@ -1,49 +1,87 @@
-package org.example;
+package example;
 
 import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ClientsRep implements ClRepository{
+public class ClientsRepozitory implements IRepo<Clients> {
+    @Override
+    public int insert(Clients client) throws SQLException {
+        String str = String.format("INSERT INTO clients (surname, name, id_master) VALUES ('%s', '%s', %s)",
+                client.getName(),
+                client.getSurname(),
+                client.getIdMasters().getId());
+
+        client.setDelete(false);
+        this.executeRequest(str);
+        try (ResultSet ms = this.getStatement(this.connectToDB()).executeQuery("SELECT MAX(id) FROM Clients")) {
+            while (ms.next()) {
+                return ms.getInt(1);
+            }
+            return -1;
+        }
+    }
+
+    private void executeRequest(String request) throws SQLException {
+        Statement stmt = this.getStatement(this.connectToDB());
+        stmt.execute(request);
+        stmt.close();
+    }
 
     @Override
-    public List<Clients> getAllClients() throws SQLException {
-        List<Clients> listClients = new ArrayList<>();
+    public void delete(Clients client) throws SQLException {
+        String str = String.format("UPDATE Clients SET isDelete = true WHERE id = %s" , client.getId());
+        this.executeRequest(str);
+    }
+    @Override
+    public void update(Clients client, int id) throws SQLException {
+        String str = String.format("UPDATE clients SET id = %s, surname = '%s', name = '%s', id_master = %s WHERE id = %s" ,
+                client.getId(),
+                client.getSurname(),
+                client.getName(),
+                client.getIdMasters().getId(),
+                id);
+        Statement stmt = this.getStatement(this.connectToDB());
+        stmt.execute(str);
+        stmt.close();
+    }
+    @Override
+    public List<Clients> getList() throws SQLException {
+        Statement stmt = this.getStatement(this.connectToDB());
+        ResultSet rs = stmt.executeQuery("SELECT cl.id, cl.surname ,cl.name, cl.id_master, st.id, st.surname,st.name, st.isDelete, cl.isDelete FROM Clients  AS cl JOIN Masters AS st ON cl.id_master = st.ID");
+
+        List<Clients> clients = new ArrayList<>();
+        while(rs.next()) {
+            clients.add(new Clients(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getString("surname"),
+                    new Masters(
+                            rs.getInt("Masters.id"),
+                            rs.getString("Masters.name"),
+                            rs.getString("Masters.surname"),
+                            rs.getBoolean("Masters.ISDELETE")),
+                    rs.getBoolean("Clients.isDelete")));
+        }
+        this.closeConnection(stmt);
+        return clients;
+    }
+    @Override
+    public Connection connectToDB() throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/mem:salon", "admin", "");
-        if (conn == null) {
-            System.out.println("Нет соединения с БД!");
+        if (conn==null) {
+            System.out.println("Error with connection with DataBase!");
             System.exit(0);
         }
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("");
-        while (rs.next()) {
-            int id = rs.getInt("id_client");
-            String name = rs.getString("name");
-            String surname = rs.getString("surname");
-            Clients client = new Clients(id, name, surname);
-            listClients.add(client);
-        }
+        return conn;
+    }
+    @Override
+    public Statement getStatement(Connection conn) throws SQLException {
+        return conn.createStatement();
+    }
+    public void closeConnection(Statement stmt) throws SQLException {
         stmt.close();
-        return listClients;
     }
-
-    @Override
-    public Clients getIdClient(int id) {
-        return null;
-    }
-
-    @Override
-    public void addClient(Clients client) {
-
-    }
-
-    @Override
-    public void updateClient(Clients client) {
-
-    }
-
-    @Override
-    public void deleteClient(int id) {
-
-    }
-}
