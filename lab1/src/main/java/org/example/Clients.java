def84c129dce3bdfc56fb011f7a25d8e9558d163
Voduckra <135720709+Voduckra@users.noreply.github.com>
2024-04-09 20:01:35 +0500

    Update Clients.java

--- lab1/src/main/java/org/example/Clients.java
+++ lab1/src/main/java/org/example/Clients.java
@@ -1,22 +1,26 @@
-package org.example;
+package example;
 
 public class Clients {
-    private int id_client;
-    private String name;
+    private Boolean isDelete;
+    private Integer id;
     private String surname;
+    private String name;
+    private Masters id_masters;
 
-    public Clients(int id_client, String name, String surname) {
-        this.id_client = id_client;
-        this.name = name;
+    public Clients(Integer id, String surname, String name, Masters id_masters, Boolean isDelete) {
+        this.id = id;
         this.surname = surname;
+        this.name = name;
+        this.id_masters = id_masters;
+        this.isDelete = isDelete;
     }
 
-    public int getId_client() {
-        return id_client;
+    public int getId() {
+        return id;
     }
 
-    public void setId_client(int in_client) {
-        this.id_client = in_client;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -33,5 +37,20 @@ public class Clients {
 
     public void setSurname(String surname) {
         this.surname = surname;
+    }
+
+    public Masters getIdMasters() {
+        return id_masters;
+    }
+
+    public void setIdMasters(Masters id_masters) {
+        this.id_masters = id_masters;
+    }
+
+    public boolean isDelete() {
+        return isDelete;
+    }
+    public void setDelete(boolean deleted) {
+        isDelete = deleted;
     }
 }
